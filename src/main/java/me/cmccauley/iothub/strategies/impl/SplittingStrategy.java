247b7e5e8b9126c1a2f631ff894a3835e0c0c013
Charles McCauley <charles.mccauley.5@gmail.com>
2018-01-25 15:49:01 -0500

    strategy

--- src/main/java/me/cmccauley/iothub/strategies/impl/SplittingStrategy.java
+++ src/main/java/me/cmccauley/iothub/strategies/impl/SplittingStrategy.java
@@ -5,6 +5,8 @@ import me.cmccauley.iothub.strategies.PublishStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
 @Component
 public class SplittingStrategy implements PublishStrategy{
 
@@ -13,6 +15,26 @@ public class SplittingStrategy implements PublishStrategy{
 
     @Override
     public void publish(String message) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.next();
 
+        int steps = 0;
+
+        while(!isPalindrome(inputString))
+        {
+            String reverse = new StringBuilder(inputString).reverse().toString();
+            int numericValue = Integer.parseInt(inputString) + Integer.parseInt(reverse);
+            inputString = String.valueOf(numericValue);
+        }
+
+        System.out.println(inputString + " " + steps);
+
+        String unbalanced = "()()(";
+
+    }
+
+    public boolean isPalindrome(String inputString){
+        return inputString.equals(new StringBuilder(inputString).reverse().toString());
     }
 }
